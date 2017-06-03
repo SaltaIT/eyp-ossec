09fb76c1dc704cbc53aed64fc73b8342cc957513
Jordi Prats <jprats@systemadmin.es>
2017-06-03 09:54:15 +0200

    variables logformat

--- manifests/server/sharedagent/localfile.pp
+++ manifests/server/sharedagent/localfile.pp
@@ -5,5 +5,25 @@
 #    <location>/var/log/messages</location>
 #  </localfile>
 #
-define ossec::server::sharedagent::localfile() {
+# {% if item.localfiles is defined %}
+#   <!-- Files to monitor (localfiles) -->
+# {% for localfile in item.localfiles %}
+#   <localfile>
+#      <log_format>{{ localfile.format }}</log_format>
+#      {% if localfile.command is defined %}
+#      <command>{{ localfile.command }}</command>
+#      {% else %}
+#      <location>{{ localfile.location }}</location>
+#      {% endif %}
+#   </localfile>
+# {% endfor %}
+# {% endif %}
+#
+#
+define ossec::server::sharedagent::localfile(
+                                              $location,
+                                              $logformat = undef,
+                                              $command   = undef,
+                                              $os        = $name,
+                                            ) {
 }
