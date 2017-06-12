c8004fa5c3af3082e2bc25f1cbc69705db9236dc
Jordi Prats <jprats@systemadmin.es>
2017-06-12 12:36:58 +0200

    typo

--- manifests/server/ignore.pp
+++ manifests/server/ignore.pp
@@ -8,14 +8,14 @@ define ossec::server::ignore(
   if(!defined(Concat::Fragment["server ignore header"]))
   {
     concat::fragment{ "server ignore header":
-      target  => '/var/ossec/etc/shared/agent.conf',
+      target  => '/var/ossec/etc/ossec-server.conf',
       order   => "11a",
       content => "\n    <!-- Files/directories to ignore -->\n",
     }
   }
 
   concat::fragment{ "server ignore ${file}":
-    target  => '/var/ossec/etc/shared/agent.conf',
+    target  => '/var/ossec/etc/ossec-server.conf',
     order   => "11b",
     content => template("${module_name}/shared_agent/03_ignore.erb"),
   }
