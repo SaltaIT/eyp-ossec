d731d238d0cbcfeac532f7dc506b6e9b848608b2
Jordi Prats <jordi.prats@gmail.com>
2019-05-07 10:02:41 +0200

    centos 7 only

--- manifests/params.pp
+++ manifests/params.pp
@@ -16,31 +16,10 @@ class ossec::params {
     {
       case $::operatingsystemrelease
       {
-        /^[5-7].*$/:
+        /^[7].*$/:
         {
         }
         default: { fail("Unsupported RHEL/CentOS version! - ${::operatingsystemrelease}")  }
-      }
-    }
-    'Debian':
-    {
-      case $::operatingsystem
-      {
-        'Ubuntu':
-        {
-          case $::operatingsystemrelease
-          {
-            /^14.*$/:
-            {
-            }
-            /^16.*$/:
-            {
-            }
-            default: { fail("Unsupported Ubuntu version! - ${::operatingsystemrelease}")  }
-          }
-        }
-        'Debian': { fail('Unsupported')  }
-        default: { fail('Unsupported Debian flavour!')  }
       }
     }
     default: { fail('Unsupported OS!')  }
