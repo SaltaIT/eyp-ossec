7ffc986f238e4a9abb1561931b4d6aedcbe90301
Jordi Prats <jordi.prats@gmail.com>
2019-05-07 09:44:23 +0200

    litn

--- manifests/agent/service.pp
+++ manifests/agent/service.pp
@@ -1,12 +1,5 @@
 class ossec::agent::service inherits ossec::agent {
 
-  #
-  validate_bool($ossec::agent::manage_docker_service)
-  validate_bool($ossec::agent::manage_service)
-  validate_bool($ossec::agent::service_enable)
-
-  validate_re($ossec::agent::service_ensure, [ '^running$', '^stopped$' ], "Not a valid daemon status: ${ossec::service_ensure}")
-
   $is_docker_container_var=getvar('::eyp_docker_iscontainer')
   $is_docker_container=str2bool($is_docker_container_var)
 
